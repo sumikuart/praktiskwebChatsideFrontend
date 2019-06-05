46b11475bb67a371070126dea85b8d5f7dcdb086
sommerch <chen0058@VID.net.local>
2019-06-05 13:03:20 +0200

    update/useradmin

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import '../App.css';
-import Logo from '../img/logo.png'
+
+// import Logo from '../img/logo.png'
 
 import LoginOpret from "./loginOpret"
 
@@ -12,19 +13,18 @@ export default class Navbar extends Component {
  
 render() {
 return (
-<Router>
 
     <nav className="nav">
     {/* <img className="logo" src={Logo} alt=""/> */}
         <ul className="ul"> 
-       
-            <li className="li"> <Link To="./">Home</Link></li>
-            <li className="li"> <Link To="./chat">Chat</Link></li>
+   
+            <li className="li"> <Link To="/">Home</Link></li>
+            <li className="li"> <Link To="/chat">Chat</Link></li>
             <li className="li"> <LoginOpret/> </li>
-          
+            <li className="li"> <Link To="/founder">Founder</Link></li>
         </ul>
     </nav>
-</Router>
+
 )
 }
 }
